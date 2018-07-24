a921018b15ef0e0e99cdf025a023ffbc6520d0ff
koechkevin <koechkevin92@gmail.com>
2018-07-24 08:35:31 +0300

    unittest improved

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ connection=connection=db_model.connection
 
 def authorize(token):
     output=True
-    if token is None or token=='':
+    if token is None or token.strip()=='':
         return False
     sql="select token from blacklist where token='"+token+"';"
     cursor=connection.cursor()
@@ -41,47 +41,57 @@ def home():
 
 @app.route('/api/v2/register',methods=['POST'])
 def register():
-    connection.commit()
-    data = request.get_json()
-    fname=data['fname']
-    lname=data['lname']
-    email=data['email']
-    username=data['username']
-    password=hashlib.sha256(base64.b64encode(bytes(data['password'],'utf-8'))).hexdigest()
-    confirm_password=hashlib.sha256(base64.b64encode(bytes(data['cpassword'],'utf-8'))).hexdigest()
-    cursor=connection.cursor()
-    sql1="INSERT INTO users \
-    (name,email,username,password) VALUES \
-    ('"+fname+" "+lname+"','"+email+"',\
-    '"+username+"','"+password+"');"
-    cursor.execute("select * from users where username = '"+username+"';")
-    if cursor.fetchone() is not None:
+    try:
         connection.commit()
-        return jsonify({"message":"such user already exists"}),409
-    elif password != confirm_password:
-        return jsonify({"message":"password and confirm password do not match"}),403
-    elif not validEmail(email):
-        return jsonify("please enter a valid email"),403
-    cursor.execute(sql1)
-    connection.commit()
-    return jsonify({"message":"You registered succesfully"}),200
-
+        data = request.get_json()
+        fname=data['fname']
+        lname=data['lname']
+        email=data['email']
+        username=data['username']
+        password=hashlib.sha256(base64.b64encode\
+        (bytes(data['password'],'utf-8'))).hexdigest()
+        confirm_password=hashlib.sha256(base64.b64encode\
+        (bytes(data['cpassword'],'utf-8'))).hexdigest()
+        if fname.strip()=='' or lname.strip()=='' or password.strip()=='':
+            return jsonify('Fields cannot be empty'),422
+        cursor=connection.cursor()
+        sql1="INSERT INTO users \
+        (name,email,username,password) VALUES \
+        ('"+fname+" "+lname+"','"+email+"',\
+        '"+username+"','"+password+"');"
+        cursor.execute("select * from users where username = '"+username+"';")
+        if cursor.fetchone() is not None:
+            connection.commit()
+            return jsonify({"message":"such user already exists"}),409
+        elif password != confirm_password:
+            return jsonify({"message":"password and confirm password do not match"}),403
+        elif not validEmail(email):
+            return jsonify("please enter a valid email"),403
+        cursor.execute(sql1)
+        connection.commit()
+        return jsonify({"message":"You registered succesfully"}),200
+    except KeyError:
+        return jsonify('fname,lname,email,username,password,cpassword should be provided'),422
 @app.route('/api/v2/login',methods=['POST'])
 def login():
-    username=request.get_json()['username']
-    password=hashlib.sha256(base64.b64encode(bytes(request.get_json()['password'],'utf-8'))).hexdigest()
-    payload={}    
-    sql="select * from users where username='"+username+"' and password ='"+password+"';"
-    cursor=connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchone()
-    if result is None:
-        return jsonify({"message":"invalid credentials"}),401
-    payload={"userID":result[0],"username":username,\
-             "exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=15)}
-    connection.commit()
-    token=jwt.encode(payload,app.secret_key)
-    return jsonify({"token":token.decode('utf-8')}),200
+    try:
+        username=request.get_json()['username']
+        password=hashlib.sha256(base64.b64encode\
+        (bytes(request.get_json()['password'],'utf-8'))).hexdigest()
+        payload={}    
+        sql="select * from users where username='"+username+"' and password ='"+password+"';"
+        cursor=connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        if result is None:
+            return jsonify({"message":"invalid credentials"}),401
+        payload={"userID":result[0],"username":username,\
+                 "exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=15)}
+        connection.commit()
+        token=jwt.encode(payload,app.secret_key)
+        return jsonify({"token":token.decode('utf-8')}),200
+    except KeyError:
+        return jsonify('username and password should be provided in a json format'),422    
 
 
 
@@ -114,6 +124,8 @@ def post_entry():
             data=request.get_json()
             title=data['title']
             entry=data['entry']
+            if title.strip()=='' or entry.strip()=='':
+                return jsonify('title and entry cannot be empty'),422            
             userID=jwt.decode(request.args.get('token'), app.secret_key)['userID']
             cursor=connection.cursor()
             sql="insert into entries \
@@ -124,7 +136,9 @@ def post_entry():
         except jwt.ExpiredSignatureError:
             return jsonify('your token expired please login again'),401
         except jwt.InvalidTokenError:
-            return jsonify('invalid token'),403        
+            return jsonify('invalid token'),403
+        except KeyError:
+            return jsonify('provide title and entry to be saved'),422
     else:
         return jsonify("you are out of session"),401
 
@@ -160,15 +174,16 @@ def view_entry(entryID):
             cursor.execute(sql)
             result=cursor.fetchone()
             if result is None:
-                return jsonify("you are not authorized to perform this operation"),401
+                return jsonify("entry id "+str(entryID)+" is \
+                not part of your entries . you can only view your entries"),401
             connection.commit()
             return  jsonify(result[0],result[1],result[2],result[4]),200
         except jwt.ExpiredSignatureError:
-            return jsonify('your token expired please login again')
+            return jsonify('your token expired please login again'),403
         except jwt.InvalidTokenError:
-            return jsonify('invalid token please login to get a new token')        
+            return jsonify('invalid token please login to get a new token'),403        
     else:
-        return jsonify("you are out of session")    
+        return jsonify("you are out of session"),401    
 
 @app.route("/api/v2/delete_entry/<int:entryId>",methods=["DELETE"])
 def delete_entry(entryId):
@@ -201,6 +216,8 @@ def modify_entry(entryId):
         userID=jwt.decode(request.args.get('token'), app.secret_key)['userID']
         title=request.get_json()['title']
         entry=request.get_json()['entry']
+        if title.strip()=='' or entry.strip()=='':
+            return jsonify('title and entry cannot be empty'),422
         today=str(datetime.datetime.today()).split()
         if authorize(request.args.get('token')):
             cursor=connection.cursor()
@@ -220,20 +237,25 @@ def modify_entry(entryId):
             return jsonify("you are out of session")
     except jwt.ExpiredSignatureError:
         return jsonify('your token expired please login again')
+    except KeyError:
+        return jsonify('provide new title and new entry to replace'),422
     except jwt.InvalidTokenError:
         return jsonify('invalid token please login to get a new token')        
 
 
 @app.route("/api/v2/logout",methods=['GET'])
 def logout():
-    token=request.args.get('token')
-    clearBlacklist="DELETE FROM blacklist WHERE time < NOW() - INTERVAL '30 minutes';"
-    sql=" INSERT INTO blacklist(token)VALUES ('"+token+"');"
-    cursor=connection.cursor()
-    cursor.execute(clearBlacklist)
-    cursor.execute(sql)
-    connection.commit()
-    return jsonify("you have been successfully logged out.Token invalidated"),200
+    try:
+        token=request.args.get('token')
+        clearBlacklist="DELETE FROM blacklist WHERE time < NOW() - INTERVAL '30 minutes';"
+        sql=" INSERT INTO blacklist(token)VALUES ('"+token+"');"
+        cursor=connection.cursor()
+        cursor.execute(clearBlacklist)
+        cursor.execute(sql)
+        connection.commit()
+        return jsonify("you have been successfully logged out.Token invalidated"),200
+    except TypeError:
+        return jsonify('you can only logout if you were logged in'),422
 
 if __name__=='__main__':
     db_model.create_table()
--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from app import *
 class Test_app(unittest.TestCase):
     def test_authorize(self):
@@ -12,6 +13,7 @@ class Test_app(unittest.TestCase):
         response2 = test.get('/api/v2')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response2.status_code, 404)
+        self.assertEqual(response.get_data(as_text=True),'welcome to my diary')
     def test_register(self):  
         self.assertEqual\
             (app.test_client().get('/api/v2/register').status_code,405)
@@ -23,6 +25,7 @@ class Test_app(unittest.TestCase):
         res=t.post('/api/v2/register',json={"fname":"kevin",\
         "lname":"koech","email":"kkkoech","username":"kibitoks",\
         "password":"1234","cpassword":"1234"})
+        self.assertEqual(json.loads(res.get_data(as_text=True)),'please enter a valid email')
         self.assertEqual(res.status_code,403)
     def test_login(self):
         tester=app.test_client()
@@ -33,6 +36,30 @@ class Test_app(unittest.TestCase):
             response=test.post('/api/v2/create_entry?token=',json={"title":"kevins first","entry":"its awesome"})
             self.assertEqual(response.status_code,401)
             self.assertEqual(test.get('/api/v2/create_entry?token=').status_code,405)
+    def test_entries(self):
+        t=app.test_client()
+        r=t.get('/api/v2/entries')
+        self.assertEqual(r.status_code,200)
+        self.assertEqual(t.post('/api/v2/entries').status_code,405)
+    def test_logout(self)    :
+        t=app.test_client().get('/api/v2/logout').status_code
+        self.assertEqual(t,422)
+        te=app.test_client().get('/api/v2/logou').status_code
+        self.assertEqual(te,404)
+    def test_delete_entry(self):
+        with app.test_client() as test:
+            r=test.get('/api/v2/delete_entry').status_code
+            self.assertEqual(r,404)
+    def test_modify_entry(self):
+        modify=app.test_client().post('/api/v2/modify_entry/3',json={}).status_code
+        self.assertEqual(modify,405)
+    def test_view_entry(self):
+        te=app.test_client().get('/api/v2/view_entry/8').status_code
+        self.assertEqual(te,401) 
+        response=app.test_client().get('/api/v2/view_entry/8')
+        self.assertEqual(json.loads(response.get_data(as_text=True)),"you are out of session")
+            
+            
             
 if __name__=='__main__':
     unittest.main()
