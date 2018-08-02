ec660b3e4e57bb8ed20fa7981edf32e6512e83c2
koechkevin <koechkevin92@gmail.com>
2018-08-02 08:23:47 +0300

    fix cors

--- api/v2/__init__.py
+++ api/v2/__init__.py
@@ -7,12 +7,14 @@ sys.path.insert(0, os.path.abspath(".."))
 import jwt
 
 from flask import Flask
+#from flask_cors import CORS
 from users.views import user
 from entries.views import apps
 from routes import main
 
 
 app = Flask(__name__)
+#CORS(app)
 app.secret_key = 'koech'
 
 
