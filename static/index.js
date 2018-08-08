a61c021a462c38be21a143422c40814dcf6399a5
koechkevin <koechkevin92@gmail.com>
2018-08-08 19:47:37 +0300

    use localhost server

--- static/index.js
+++ static/index.js
@@ -1,32 +1,70 @@
 function fetchIndex(){
-fetch("http://127.0.0.1:5013/api/v2").then(function(response){
-    if (!response.ok) {
-    throw Error(response.statusText);
-  }
-    else{
-    console.log(response.json());
-    }
+fetch("http://127.0.0.1:5013/api/v2/", {method:"GET", 
+headers:{"Content-Type":"application/json"}})
+.then((response) => response.json())
+.then((data)=>{
+    document.getElementById("output").innerHTML = data["message"]
 })
+.catch((err) => console.log(err))
 }
+
 function fetchRegister(){
-    var url = "http://127.0.0.1:5013/api/v2/users/register"
-    var fname = document.getElementById("fname")
-    var lname = document.getElementById("lname")
-    var username = document.getElementById("username")
-    var email = document.getElementById("email")
-    var password = document.getElementById("password")
-    var cpassword = document.getElementById("cpassword")
-    var data = {"fname":fname,"lname":lname, "username":username, "email":email,
-               "password":password, "cpassword":cpassword}
-    fetch(url, {method: 'POST',
-         body: JSON.stringify(data),
-        headers:{
-    'Content-Type': 'application/json'
+    event.preventDefault()
+    var url = "http://localhost:5013/api/v2/users/register"
+    var firstname = document.forms["register"]["fname"].value;
+    var lastname = document.forms["register"]["lname"].value;
+    var name = document.forms["register"]["username"].value;
+    var emailaddress = document.forms["register"]["email"].value;
+    var pasword = document.forms["register"]["password"].value;
+    var confirmpassword = document.forms["register"]["cpassword"].value;
+    var data = {fname:firstname,lname:lastname, username:name, email:emailaddress,
+               password:pasword, cpassword:confirmpassword};                    
+    fetch(url, {method:"POST", 
+    headers:{
+        "Content-Type":"application/json"
+}, 
+body:JSON.stringify(data)
+    }).then((res)=>res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log('error:',err));
+    
         }
-  }).then(function(response){
-      if (!response.ok){}
-      else {
-      console.log(response.json());
-      }
-  }
-  )}
+function fetchAccount(){
+    event.preventDefault()
+    const url = "http://127.0.0.1:5013/api/v2/users/register"
+    fetch(url, {method:"GET",
+headers: {"Content-Type":"application/json", 'x-access-token':"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMywidXNlcm5hbWUiOiJraWJpdG9rIiwiZXhwIjoxNTMzNzQ2NzE4fQ.VD4-Rsummo-HKHtZ5Cyl6NAzwn1kNlYU2OsK_rwadjQ"}})
+.then((resp)=>resp.json())
+.then((data) => console.log(data))
+.catch(error => console.log('error:',error));
+}
+function fetchLogin(){
+    event.preventDefault()
+    var url = "http://127.0.0.1:5013/api/v2/users/login"
+    var user = document.forms["login"]["username"].value;
+    var pass = document.forms["login"]["password"].value;
+    var data = {username:user, password:pass};
+    fetch(url, {method:"POST", 
+    headers:{
+        "Content-Type":"application/json"
+}, 
+body:JSON.stringify(data)
+    })
+    .then((res)=>res.json())
+    .then((data) => console.log(data))
+    .catch(error => console.log('error:',error));
+
+}
+function fetchNewEntry(){
+    event.preventDefault()
+    let url = "http://127.0.0.1:5013/api/v2/entries"
+    let titl = document.forms["create"]["title"].value;
+    let entr = document.forms["create"]["entry"].value;
+    let data = {title:titl, entry:entr}
+    fetch(url, {method:"POST",
+    headers: {"Content-Type":"application/json", 'x-access-token':""},
+    body:JSON.stringify(data)
+}).then((response) => response.json())
+.then((output)=>console.log(output))
+.catch((err)=>console.log(err))
+}
